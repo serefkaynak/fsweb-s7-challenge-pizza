fc4ae4b8f7b892eaaa4888a322c75a334c41ae0a
Okyanus Aydogan <okyanusaydgn@gmail.com>
2023-11-30 23:19:36 +0300

    proje güncellendi

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -1,7 +1,106 @@
 import { getByPlaceholderText } from "@testing-library/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function OrderPage() {
+  
+  const [quantity, setQuantity] = useState(1);
+  
+  const decrement = (e) => {
+    e.preventDefault();
+    ((quantity > 1)? setQuantity(quantity-1): setQuantity(quantity));
+    // if(quantity > 1){
+    //   setQuantity(quantity-1)
+    // }
+    // else{
+    //   setQuantity(quantity);
+    // }
+    
+  }
+    
+  const [extrasData, setextrasData] = useState({
+    Pepperoni: false,
+    Tavuk:false,
+    Misir: false,
+    Sarimsak: false,
+    Ananas:false,
+    Sosis:false,
+    Sogan:false,
+    Salam:false,
+    Biber:false,
+    Kabak:false,
+    Jambon:false,
+    Domates:false,
+    Jalepeno:false,
+    Sucuk:false, 
+  });
+
+//  console.log( Object.keys(extrasData).length)
+
+  const handleChange = (e) => {
+    // let name = e.target.name;
+    // let value = e.target.value;
+    let { name, type, checked } = e.target;
+ 
+  
+     // const selectedExtrasCount = newExtras.filter((item) => item.check).length;
+  // if (selectedExtrasCount <= 10) {
+  //   setFormData({
+  //     ...formData,
+  //     extras: newExtras,
+  //   });
+    if(Object.values(extrasData).filter( e => e=== true).length  <= 9 ){
+      setextrasData({
+        ...extrasData,
+        [name]: type === "checkbox" 
+         ? true
+         : true,
+      });
+    }else{
+      setextrasData({
+        ...extrasData,
+        [name]: type === "checkbox" ? false : true,
+      });
+    }
+  };
+ 
+  useEffect(
+    () => {
+      setextrasData(extrasData);
+      console.log(extrasData)
+    },[extrasData]
+  )
+
+    const increment = (e) => {
+      e.preventDefault();
+      setQuantity(quantity+1)}
+  
+  const [quantityPizzaPrice, setQuantityPizzaPrice] = useState(85.50);
+
+  useEffect(
+    () => {
+      setQuantityPizzaPrice(quantity * 85.50)
+    },[quantity]
+  )
+
+  const [total, setTotal] = useState(quantityPizzaPrice);
+
+  const [selectionPrice,setSelectionPrice] = useState(0);
+  
+  useEffect(
+    () => {
+      let selected= Object.values(extrasData).filter( e => e=== true).length 
+      if(selected <= 10){
+      setSelectionPrice(selected*5)}
+  },[extrasData])
+
+
+
+
+  useEffect(
+    () => {
+      setTotal(quantityPizzaPrice + selectionPrice)  
+  },[quantityPizzaPrice, selectionPrice])
+
   return (
     <div>
       <div className="orderPageHeader">
@@ -59,7 +158,7 @@ export default function OrderPage() {
                 <h3>
                   Hamur Seç <span style={{ color: "red" }}>*</span>
                 </h3>
-                <label>
+                <label>                                                                                                                                     
                   <select>
                     <option>State oluşturulacak</option>
                     <option>İnce</option>
@@ -75,46 +174,46 @@ export default function OrderPage() {
             </div>
             <div className="extra-checkbox-selection">
               <label>
-                <input type="checkbox" /> Pepperoni{" "}
+                <input name="Pepperoni" type="checkbox" checked={extrasData.Pepperoni} onChange={handleChange}/> Pepperoni
               </label>
               <label>
-                <input type="checkbox" /> Tavuk Izgara
+                <input name="Tavuk" type="checkbox" checked={extrasData.Tavuk} onChange={handleChange} /> Tavuk Izgara
               </label>
               <label>
-                <input type="checkbox" /> Mısır
+                <input name="Misir" type="checkbox" checked={extrasData.Misir} onChange={handleChange}/> Mısır
               </label>
               <label>
-                <input type="checkbox" /> Sarımsak{" "}
+                <input name="Sarimsak"  type="checkbox" checked={extrasData.Sarimsak} onChange={handleChange}/> Sarımsak
               </label>
               <label>
-                <input type="checkbox" /> Ananas
+                <input name="Ananas" type="checkbox" checked={extrasData.Ananas} onChange={handleChange}/> Ananas
               </label>
               <label>
-                <input type="checkbox" /> Sosis
+                <input name="Sosis" type="checkbox" checked={extrasData.Sosis} onChange={handleChange}/> Sosis
               </label>
               <label>
-                <input type="checkbox" /> Soğan
+                <input name="Sogan" type="checkbox" checked={extrasData.Sogan} onChange={handleChange}/> Soğan
               </label>
               <label>
-                <input type="checkbox" /> Sucuk
+                <input name="Salam" type="checkbox" checked={extrasData.Salam} onChange={handleChange}/> Salam
               </label>
               <label>
-                <input type="checkbox" /> Biber{" "}
+                <input name="Biber" type="checkbox" checked={extrasData.Biber} onChange={handleChange}/> Biber
               </label>
               <label>
-                <input type="checkbox" /> Kabak
+                <input name="Kabak" type="checkbox" checked={extrasData.Kabak} onChange={handleChange}/> Kabak
               </label>
               <label>
-                <input type="checkbox" /> Kanada Jambonu
+                <input name="Jambon" type="checkbox" checked={extrasData.Jambon} onChange={handleChange}/> Kanada Jambonu
               </label>
               <label>
-                <input type="checkbox" /> Domates{" "}
+                <input name="Domates" type="checkbox" checked={extrasData.Domates} onChange={handleChange} /> Domates
               </label>
               <label>
-                <input type="checkbox" /> Jalepeno
+                <input name="Jalepeno" type="checkbox" checked={extrasData.Jalepeno} onChange={handleChange}/> Jalepeno
               </label>
               <label>
-                <input type="checkbox" /> Sucuk
+                <input name="Sucuk" type="checkbox" checked={extrasData.Sucuk} onChange={handleChange}/> Sucuk
               </label>
             </div>
             <div>
@@ -132,20 +231,25 @@ export default function OrderPage() {
             </div>
             <div className="result">
               <div className="quantify">
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
+                <button  onClick={
+                    decrement
+                }>-</button>
+                <span style={{margin:"10px"}}>{quantity}</span>
+                <button onClick={increment}>+</button>
               </div>
               
               <div className="totalPrice">
                 <h3>Sipariş Toplamı</h3>
 
                 <div className="price-area">
-                  <div>
-                    Seçimler <span>25.50Tl</span>
+                <div className="price-wrapper">
+                    Pizza Adet Toplamı : <span>{quantityPizzaPrice}₺</span>
                   </div>
-                  <div>
-                    Toplam <span>125.50Tl</span>
+                  <div className="price-wrapper">
+                    Seçimler <span>{selectionPrice}₺</span>
+                  </div>
+                  <div className="price-wrapper">
+                    Toplam <span>{total}₺</span>
                   </div>
                 </div>
 
